616103e23b21ce2e1105081d2a561dbf46f91eca
FirstStrik3 <sri.lakshman.89@gmail.com>
2026-08-31 23:06:06 +0530

    SimCafe Completed Project

---
+++ Assets/_Game_Assets_/KitchenChaos_143_Polish/Assets/Scripts/UI/GameOverScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour
+{
+
+
+    [SerializeField] private Button mainMenuButton;
+
+    private void Awake()
+    {
+        mainMenuButton.onClick.AddListener(() => {
+            Loader.Load(Loader.Scene.MainMenuScene);
+        });
+
+        Time.timeScale = 1f;
+    }
+
+}
